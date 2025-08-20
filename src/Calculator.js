e9ea374c436de4e5e8ac209f957061c4920d8acb
abduyus <yusuf.abdurrasheed@outlook.com>
2025-08-20 15:01:25 +0500

    fixing the bug caused by the state changing when allowSound is toggled

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,4 +1,4 @@
-import {memo, useCallback, useEffect, useState} from 'react';
+import {memo, useEffect, useState} from 'react';
 import clickSound from './ClickSound.m4a';
 
 function Calculator({ workouts, allowSound }) {
@@ -9,16 +9,25 @@ function Calculator({ workouts, allowSound }) {
 
   const [duration, setDuration] = useState(0)
 
-  const playSound = useCallback(function () {
-    if (!allowSound) return;
-    const sound = new Audio(clickSound);
-    sound.play();
-  }, [allowSound])
+  // const playSound = useCallback(function () {
+  //   if (!allowSound) return;
+  //   const sound = new Audio(clickSound);
+  //   sound.play();
+  // }, [allowSound])
 
   useEffect(function() {
     setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak)
-    playSound()
-  }, [number, sets, speed, durationBreak, playSound])
+
+  }, [number, sets, speed, durationBreak])
+
+  useEffect(() => {
+    const playSound = function () {
+        if (!allowSound) return;
+        const sound = new Audio(clickSound);
+        sound.play();
+      }
+      playSound()
+  }, [allowSound, duration]);
 
   // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
   const mins = Math.floor(duration);
@@ -27,12 +36,10 @@ function Calculator({ workouts, allowSound }) {
 
   function handleInc() {
     setDuration((duration) => Math.floor(duration + 1))
-    playSound()
   }
 
   function handleDec() {
     setDuration((duration) => duration > 1 ? Math.ceil(duration) - 1 : 0 )
-    playSound()
   }
 
   return (
